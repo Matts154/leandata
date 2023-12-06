efacb81b4d343e044912467cf646a26432c83dae
Matt <bored154@gmail.com>
2023-12-06 15:56:32 -0800

    Initial commit of vue3 starter

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>{{ greeting }} <slot /></p>
+  <p>Counter: {{ count }}</p>
+  <p>Double: {{ double }}</p>
+  <button @click="increment">Increment</button>
+</template>
+
+<script>
+import { mapActions, mapState, mapStores } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+export default {
+  props: {
+    greeting: {
+      type: String,
+      default: 'Hello'
+    }
+  },
+  computed: {
+    // other computed properties
+    // ...
+    // gives access to this.counterStore and this.userStore
+    ...mapStores(useCounterStore),
+    // gives read access to this.count and this.double
+    ...mapState(useCounterStore, ['count', 'double'])
+  },
+  methods: {
+    // gives access to this.increment()
+    ...mapActions(useCounterStore, ['increment'])
+  }
+}
+</script>
+
+<style></style>
