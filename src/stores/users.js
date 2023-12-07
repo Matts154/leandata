9469608dbc29e7d5dc9a9a0b3ef0ae9144530a0b
Matt <bored154@gmail.com>
2023-12-06 18:06:18 -0800

    Removed count getter from boilerplate. Fixed formatting.

--- src/stores/users.js
+++ src/stores/users.js
@@ -2,10 +2,9 @@ import { defineStore } from 'pinia';
 import User from '../models/User.js';
 
 export const useUsersStore = defineStore('user', {
-  state: () => ({ users: [] }),
-  getters: {
-    double: (state) => state.count * 2,
-  },
+  state: () => ({
+    users: [],
+  }),
   actions: {
     create(firstName, lastName) {
       this.users.push(new User(firstName, lastName));
