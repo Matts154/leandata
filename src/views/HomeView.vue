613665828ffefee286ad869fc67fdb35eeccbd59
Matt <bored154@gmail.com>
2023-12-07 11:06:41 -0800

    Added whitespace to home view.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,12 @@
   <main>
     <v-container>
       <v-row>
-        <v-col cols="12">
+        <v-col>
           <h1>LeanData Take Home Project</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
           <p>Create a web application that allows you to manage users and expenses for each user. You can use any front-end framework and library to complete this assignment.</p>
         </v-col>
       </v-row>
