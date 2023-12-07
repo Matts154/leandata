a3292c47a73fd12168f0d0d338208781990b7f64
Matt <bored154@gmail.com>
2023-12-06 18:56:05 -0800

    Fixed footer styles.

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,15 @@ import { RouterLink, RouterView } from 'vue-router'
     </v-container>
   </main>
 
-  <footer>2023 &#8212; Matthew Stephenson</footer>
+  <footer>
+    <v-container>
+      <v-row justify="center" no-gutters>
+        <v-col class="text-center mt-4" cols="12">
+          2023 &#8212; Matthew Stephenson
+        </v-col>
+      </v-row>
+    </v-container>
+  </footer>
 </template>
 
 <style scoped></style>
