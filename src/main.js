b36c8b832e0f716adc37e1f96a29fb868284d35b
Matt <bored154@gmail.com>
2023-12-19 15:35:37 -0800

    Added test user data to users and expenses table using faker data. Moved expenses listener to app due to chicken and egg problem with state.

--- src/main.js
+++ src/main.js
@@ -16,9 +16,11 @@ const vuetify = createVuetify({
   directives
 })
 
+const pinia = createPinia();
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
